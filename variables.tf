6220e64e84f204efc16180ceadeabec8d60aebc4
Balsir <jubo.stehlik@gmail.com>
2022-11-21 12:12:58 +0100

    feat(variables.tf): Update default Helm chart version

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "4.2.1"
+  default     = "4.5.1"
   description = "Version of the Helm chart"
 }
 
