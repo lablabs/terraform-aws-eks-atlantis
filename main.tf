2e494ab05da9d0260470b4213d3618ccc83d4467
Attila Sovak <attila.sovak@gmail.com>
2025-07-31 08:19:09 +0200

    Migrate to universal addon (Atlantis) (#4)
    * feat(atlantis): remove old files
    
    * feat(atlantis): add base files
    
    * feat(atlantis): add docs and examples, README, values
    
    * feat(atlantis): add ignore
    
    * feat(atlantis): change versions
    
    * feat(atlantis): fix versions and add outputs and migrations
    
    * feat(atlantis): use latest pre-commit workflow
    
    * feat(atlantis): use more general output
    
    * feat(atlantis): fix docs and migrations to be generic
    
    * feat(atlantis): add support for custom aws directory definition

---
+++ main.tf
@@ -0,0 +1,40 @@
+/**
+ * # AWS EKS Atlantis Terraform module
+ *
+ * A Terraform module to deploy [Atlantis](https://www.runatlantis.io) on Amazon EKS cluster.
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-atlantis/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-atlantis/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-atlantis/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-atlantis/actions/workflows/pre-commit.yml)
+ */
+locals {
+  addon = {
+    name = "atlantis"
+
+    helm_chart_version = "5.18.0"
+    helm_repo_url      = "https://runatlantis.github.io/helm-charts"
+  }
+
+  addon_irsa = {
+    (local.addon.name) = {}
+  }
+
+  addon_values = yamlencode({
+    serviceAccount = {
+      create = module.addon-irsa[local.addon.name].service_account_create
+      name   = module.addon-irsa[local.addon.name].service_account_name
+      annotations = module.addon-irsa[local.addon.name].irsa_role_enabled ? {
+        "eks.amazonaws.com/role-arn" = module.addon-irsa[local.addon.name].iam_role_attributes.arn
+      } : tomap({})
+    }
+    aws = var.atlantis_enable_aws_profiles ? {
+      directory = var.atlantis_aws_directory
+      config = templatefile("${path.module}/templates/.aws/config", {
+        override_default_irsa_profile = var.atlantis_override_default_aws_profile
+        role_arn                      = module.addon-irsa[local.addon.name].irsa_role_enabled ? module.addon-irsa[local.addon.name].iam_role_attributes.arn : ""
+        profiles                      = var.atlantis_aws_profiles
+      })
+    } : {}
+  })
+
+  addon_depends_on = []
+}
